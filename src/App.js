18f63c6b163a126c24a67e8ea36226cf29d00d0f
puci2020 <adam.pucicki97@gmail.com>
2021-04-08 19:20:28 +0200

    Add files

---
+++ src/App.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const App = () => <div>helloasdasd</div>;
+
+export default App;
---
+++ src/theme/GlobalStyle.js
@@ -0,0 +1,17 @@
+
+import {createGlobalStyle} from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    overflow-x: hidden;
+    //font-size: 10px;
+  }
+  /* body{
+    padding-top: 100px;
+  } */
+`;
+
+export default GlobalStyle;
