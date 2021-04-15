6cf76288369a85e8aa7501a22eac34aeae4f56bc
puci2020 <adam.pucicki97@gmail.com>
2021-04-15 15:56:54 +0200

    Test

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,33 +2,33 @@ import React from "react";
 import styled from "styled-components";
 
 const Wrapper = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  min-height: 90px;
-  padding: 20px;
-  border: 1px solid lightgray;
-  overflow: hidden;
-  position: ${props => props.fixed ? 'fixed': 'static'};
-  top: ${props => props.fixed ? '0': ''};
-  /* background-color: ${({ theme }) => theme.colors.background}; */
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    min-height: 90px;
+    padding: 20px;
+    border: 1px solid lightgray;
+    overflow: hidden;
+    position: ${props => props.fixed ? 'fixed' : 'static'};
+    top: ${props => props.fixed ? '0' : ''};
+        /* background-color: ${({theme}) => theme.colors.background}; */
 `;
 
 
 const RightHeader = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  min-width: 10vw;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    min-width: 10vw;
 `;
 
-const Header = ({ avatar, fixed, left, right }) => {
-  return (
-    <Wrapper fixed={fixed}>
-      {left}
-      <RightHeader>{right}</RightHeader>
-    </Wrapper>
-  );
+const Header = ({avatar, fixed, left, right}) => {
+    return (
+        <Wrapper fixed={fixed}>
+            {left}
+            <RightHeader>{right}</RightHeader>
+        </Wrapper>
+    );
 };
 
 export default Header;
